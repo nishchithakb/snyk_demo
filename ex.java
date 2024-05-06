ef968b25bd1542e46b5872606962bac3a1a2c145
nishchithakb <165895429+nishchithakb@users.noreply.github.com>
2024-05-06 18:01:52 +1000

    Create ex.java

---
+++ ex.java
@@ -0,0 +1,25 @@
+public static void main(String[] args) {
+        boolean flag = false;
+        for (int i = 0; i <= 3; i++) {
+            if (i == 0) {
+                System.out.println("0");
+            } else if (i == 1) {
+                System.out.println("1");
+                continue;
+            } else if (i == 2) {
+                System.out.println("2");
+                flag = true;
+            } else if (i == 3) {
+                System.out.println("3");
+                break;
+            } else if (i == 4) {
+                System.out.println("4");
+            }
+            System.out.println("Hello");
+        }
+        if (flag) {
+            System.out.println("World");
+            return;
+        }
+        System.out.println("Program");
+    }
